2d6f2cf021c261386dc9ae22874d27b72816e61f
steve park <stevefordev@gmail.com>
2018-04-13 00:23:02 +0900

    add vo

--- src/com/coddington/poom/vo/Schedule.java
+++ src/com/coddington/poom/vo/Schedule.java
@@ -1,5 +1,47 @@
 package com.coddington.poom.vo;
 
-public class Schedule {
+import java.sql.Date;
 
+public class Schedule {
+	private int no,serviceNo;
+	private String serviceDay;
+	private Date serviceStartdate,serviceDate,regdate;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getServiceNo() {
+		return serviceNo;
+	}
+	public void setServiceNo(int serviceNo) {
+		this.serviceNo = serviceNo;
+	}
+	public String getServiceDay() {
+		return serviceDay;
+	}
+	public void setServiceDay(String serviceDay) {
+		this.serviceDay = serviceDay;
+	}
+	public Date getServiceStartdate() {
+		return serviceStartdate;
+	}
+	public void setServiceStartdate(Date serviceStartdate) {
+		this.serviceStartdate = serviceStartdate;
+	}
+	public Date getServiceDate() {
+		return serviceDate;
+	}
+	public void setServiceDate(Date serviceDate) {
+		this.serviceDate = serviceDate;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	
+	
 }
--- src/com/coddington/poom/vo/User.java
+++ src/com/coddington/poom/vo/User.java
@@ -1,5 +1,72 @@
 package com.coddington.poom.vo;
 
+import java.sql.Date;
+
 public class User {
 
+	private int no, coin;
+	private String email, password, name, nickName, phone, introduction, photoUrl;
+	private Date regdate;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getCoin() {
+		return coin;
+	}
+	public void setCoin(int coin) {
+		this.coin = coin;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getIntroduction() {
+		return introduction;
+	}
+	public void setIntroduction(String introduction) {
+		this.introduction = introduction;
+	}
+	public String getPhotoUrl() {
+		return photoUrl;
+	}
+	public void setPhotoUrl(String photoUrl) {
+		this.photoUrl = photoUrl;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	
+	
 }
